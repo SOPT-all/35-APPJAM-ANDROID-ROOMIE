4717ed56f50533b0501fc7d9a86a341f914b3c2d
kimsuhyeon <suhyun10201@naver.com>
2025-05-26 12:13:06 +0900

    feature/#132: imePadding 추가

--- app/src/main/java/com/wearerommies/roomie/presentation/ui/mypage/nickname/NicknameRoute.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/mypage/nickname/NicknameRoute.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -82,7 +83,8 @@ fun NicknameScreen(
         modifier = modifier
             .fillMaxSize()
             .background(RoomieTheme.colors.grayScale1)
-            .padding(paddingValues),
+            .padding(paddingValues)
+            .imePadding(),
     ) {
         RoomieTopBar(
             modifier = Modifier
