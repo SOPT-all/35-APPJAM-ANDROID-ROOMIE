41d2b7660003a95efb3037bb670f4c540cce545d
hyeumm <20221159@sungshin.ac.kr>
2025-05-28 09:22:19 +0900

    add/#134: 페이저 목데이터 추가 (그래픽은 아직 미정)

---
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/onboarding/OnboardingViewModel.kt
@@ -0,0 +1,29 @@
+package com.wearerommies.roomie.presentation.ui.onboarding
+
+import androidx.lifecycle.ViewModel
+import com.wearerommies.roomie.R
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardingViewModel @Inject constructor(
+
+) : ViewModel() {
+    val pages = listOf(
+        Triple(
+            R.string.onboarding_title1,
+            R.string.onboarding_description1,
+            R.drawable.img_home_character // 그래픽은 아직 미정
+        ),
+        Triple(
+            R.string.onboarding_title2,
+            R.string.onboarding_description2,
+            R.drawable.img_home_character
+        ),
+        Triple(
+            R.string.onboarding_title3,
+            R.string.onboarding_description3,
+            R.drawable.img_home_character
+        ),
+    )
+}
