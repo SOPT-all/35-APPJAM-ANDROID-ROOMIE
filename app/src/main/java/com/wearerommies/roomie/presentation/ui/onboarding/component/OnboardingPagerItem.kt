fe5025755f635e5a241d1a15d3e785e804d9b8e7
hyeumm <20221159@sungshin.ac.kr>
2025-05-28 09:21:27 +0900

    feature/#134: 온보딩 페이저 아이템 구현

---
+++ app/src/main/java/com/wearerommies/roomie/presentation/ui/onboarding/component/OnboardingPagerItem.kt
@@ -0,0 +1,78 @@
+package com.wearerommies.roomie.presentation.ui.onboarding.component
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.wearerommies.roomie.R
+import com.wearerommies.roomie.ui.theme.RoomieAndroidTheme
+import com.wearerommies.roomie.ui.theme.RoomieTheme
+
+@Composable
+internal fun PagerItem(
+    @StringRes title: Int,
+    @StringRes description: Int,
+    @DrawableRes image: Int,
+    screenWeight: Int,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(title),
+            color = RoomieTheme.colors.primary,
+            style = RoomieTheme.typography.obdTitle2Sb22
+        )
+
+        Spacer(
+            modifier = Modifier.height(16.dp)
+        )
+
+        Text(
+            text = stringResource(description),
+            color = RoomieTheme.colors.grayScale8,
+            style = RoomieTheme.typography.body3M14,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(
+            modifier = Modifier.height(68.dp)
+        )
+
+        Image(
+            modifier = Modifier
+                .width((screenWeight * 0.667).dp),
+            painter = painterResource(image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PagerSampleItemPreview() {
+    RoomieAndroidTheme {
+        PagerItem(
+            screenWeight = 360,
+            title = R.string.onboarding_title1,
+            description = R.string.onboarding_description1,
+            image = R.drawable.img_home_character
+        )
+    }
+}
