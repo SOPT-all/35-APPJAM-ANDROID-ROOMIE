210cddae6e47864f4aa64dc6d2410b09a98e737c
kimsuhyeon <suhyun10201@naver.com>
2025-06-05 01:24:49 +0900

    chore/#123: startDestination 수정

--- app/src/main/java/com/wearerommies/roomie/presentation/navigator/MainNavigator.kt
+++ app/src/main/java/com/wearerommies/roomie/presentation/navigator/MainNavigator.kt
@@ -39,7 +39,7 @@ class MainNavigator(
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
-    val startDestination = Route.Login
+    val startDestination = Route.Onboarding
 
     val currentTab: MainTabType?
         @Composable get() = MainTabType.find { tab ->
